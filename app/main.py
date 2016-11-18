ae151b1b6dd1bb14df08f152e40c61d40b25feea
TakeruKazama <kzm_rhacodactylus@softbank.ne.jp>
2016-11-19 01:24:00 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 def main(argv):
+    ans = [1]
+    count = 0
   for v in argv:
-    print(v)
+    for x in range(v-1):
+        n=[]
+        for i in ans:
+            count+=1
+            if p!=i:
+                n.append(count)
+                n.append(p)
+                count=1
+            p=i
+        n.append(count)
+        n.append(p)
+        ans=n
+    for v in ans:
+        print(v)
+        
