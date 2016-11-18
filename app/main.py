bf57550d90a8adfdc5b315352abce377ebe4a718
TakeruKazama <kzm_rhacodactylus@softbank.ne.jp>
2016-11-19 02:12:30 +0900

    test3

--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 def main(argv):
   ans = [1]
-  count = 0
   for v in argv:
-    for x in range(v-1):
+    for x in range(int(v)-1):
+        p = -1
         n=[]
+        count = 0
         for i in ans:
             count+=1
+            if p==-1:
+                p=i
+                count=0
             if p!=i:
                 n.append(count)
                 n.append(p)
-                count=1
+                count=0
             p=i
-        n.append(count)
-        n.append(p)
+        if p!=i:
+            n.append(count)
+            n.append(p)
+            n.append(1)
+            n.append(i)
+        else:
+            n.append(count+1)
+            n.append(p)
         ans=n
   for v in ans:
-    print(v)
+    print(v, end='')
